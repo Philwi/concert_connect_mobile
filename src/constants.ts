ad601c0a618b22952982f5f19a6ba8869c2ef740
Philipp Winkler <philrigid@gmail.com>
2024-04-22 15:18:04 +0200

    feat: init commit

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+// This is the URL of the service running the Hyperview examples
+export const ENTRY_POINT_URL = 'http://192.168.0.48:3000';
+
+// This is the URL of the service running a navigator example
+export const ENTRY_POINT_NAV_URL = 'http://192.168.0.48:3000/index_navigator.xml';
+
+export const MAIN_STACK_NAME = 'Main';
+
+export const MODAL_STACK_NAME = 'Modal';
